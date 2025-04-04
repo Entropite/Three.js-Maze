8dfd7cd58797256441c4571a90b79ff562a24841
Navjot-1313 <156293581+Navjot-1313@users.noreply.github.com>
2025-04-04 10:13:51 -0400

    Added Collision Detection

--- maze.js
+++ maze.js
@@ -1,18 +1,20 @@
 let scene, camera, renderer, controls, clock, skybox, skyboxTexture, maze;
 
 const SPEED = 1;
-
 const MOUSE_SENSITIVITY = 0.001;
 const WALL_WIDTH = 5;
 const WALL_HEIGHT = 8;
 const MAZE_SIZE = 10;
 
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+
 function init() {
   scene = new THREE.Scene();
 
-  // Creating the skybox
   const imageSkybox = "DaylightBox";
-
   function pathStrings(filename) {
     const pathBase = "https://media.githubusercontent.com/media/Entropite/Three.js-Maze/master/public/";
     const baseFilename = pathBase + filename;
@@ -51,77 +53,65 @@ function init() {
   camera.rotation.set(0, Math.PI, 0);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
-
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.domElement.id = "canvas";
   document.body.appendChild(renderer.domElement);
 
-  // Lock pointer on click
   renderer.domElement.addEventListener("click", () => {
     renderer.domElement.requestPointerLock();
   });
 
   document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
   document.addEventListener("mousemove", onMouseMove);
-  //window.addEventListener('resize', onWindowResize);
 
-  // Add lighting to the scene
   const ambientLight = new THREE.AmbientLight(0xffcccc, 0.8);
   scene.add(ambientLight);
 
   scene.add(createPlane());
 
-  // Create the maze
   maze = generateMaze(MAZE_SIZE, MAZE_SIZE);
-
-  // Make the maze have an entrance and exit
   maze[1][0] = 0;
   maze[2 * MAZE_SIZE - 1][2 * MAZE_SIZE] = 0;
 
- 
   scene.add(createHedge());
-  
+
   animate();
 }
 
 function createHedge() {
-   // Count the number of walls in the maze
-   let count = 0;
-   for (let i = 0; i < maze.length; i++) {
-     for (let j = 0; j < maze[i].length; j++) {
-       if (maze[i][j] == 1) {
-         count++;
-       }
-     }
-   }
- 
-   const hedgeGeometry = new THREE.BoxGeometry(WALL_WIDTH, WALL_HEIGHT, WALL_WIDTH);
-   const hedgeTexture = new THREE.TextureLoader().load("https://media.githubusercontent.com/media/Entropite/Three.js-Maze/master/public/Bush_Texture.jpg");
- 
-   // Make the texture repeat
-   hedgeTexture.wrapS = THREE.RepeatWrapping;
-   hedgeTexture.wrapT = THREE.RepeatWrapping;
-   hedgeTexture.repeat.set(2, 2);
- 
-   const hedgeMaterial = new THREE.MeshStandardMaterial({map: hedgeTexture});
-   
-   const hedgeMesh = new THREE.InstancedMesh(hedgeGeometry, hedgeMaterial, count);
- 
-   const tempWall = new THREE.Object3D();
-   let instanceCount = 0;
-   for (let i = 0; i < maze.length; i++) {
-     for (let j = 0; j < maze[i].length; j++) {
-       if (maze[i][j] === 1) {
-         tempWall.position.set(i * WALL_WIDTH, WALL_HEIGHT / 2, j * WALL_WIDTH);
-         tempWall.updateMatrix();
-         hedgeMesh.setMatrixAt(instanceCount, tempWall.matrix);
-         instanceCount++;
-       }
-     }
-   }
- 
-   hedgeMesh.instanceMatrix.needsUpdate = true;
-   return hedgeMesh;
+  let count = 0;
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      if (maze[i][j] == 1) count++;
+    }
+  }
+
+  const hedgeGeometry = new THREE.BoxGeometry(WALL_WIDTH, WALL_HEIGHT, WALL_WIDTH);
+  const hedgeTexture = new THREE.TextureLoader().load("https://media.githubusercontent.com/media/Entropite/Three.js-Maze/master/public/Bush_Texture.jpg");
+
+  hedgeTexture.wrapS = THREE.RepeatWrapping;
+  hedgeTexture.wrapT = THREE.RepeatWrapping;
+  hedgeTexture.repeat.set(2, 2);
+
+  const hedgeMaterial = new THREE.MeshStandardMaterial({ map: hedgeTexture });
+  const hedgeMesh = new THREE.InstancedMesh(hedgeGeometry, hedgeMaterial, count);
+
+  const tempWall = new THREE.Object3D();
+  let instanceCount = 0;
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      if (maze[i][j] === 1) {
+        tempWall.position.set(i * WALL_WIDTH, WALL_HEIGHT / 2, j * WALL_WIDTH);
+        tempWall.updateMatrix();
+        hedgeMesh.setMatrixAt(instanceCount, tempWall.matrix);
+        instanceCount++;
+      }
+    }
+  }
+
+  hedgeMesh.instanceMatrix.needsUpdate = true;
+  return hedgeMesh;
 }
 
 function createPlane() {
@@ -135,55 +125,96 @@ function createPlane() {
   return plane;
 }
 
-function animate() {
-  skybox.rotation.y += 0.0001;
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+function onKeyDown(event) {
+  switch (event.code) {
+    case "KeyW":
+      moveForward = true;
+      break;
+    case "KeyS":
+      moveBackward = true;
+      break;
+    case "KeyA":
+      moveLeft = true;
+      break;
+    case "KeyD":
+      moveRight = true;
+      break;
+  }
 }
 
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+function onKeyUp(event) {
+  switch (event.code) {
+    case "KeyW":
+      moveForward = false;
+      break;
+    case "KeyS":
+      moveBackward = false;
+      break;
+    case "KeyA":
+      moveLeft = false;
+      break;
+    case "KeyD":
+      moveRight = false;
+      break;
+  }
 }
 
 function onMouseMove(event) {
   if (document.pointerLockElement == renderer.domElement) {
     camera.rotateY(-event.movementX * MOUSE_SENSITIVITY);
-    //camera.rotateX(-event.movementY * MOUSE_SENSITIVITY);
   }
 }
 
-function onKeyDown(event) {
-  const keyCode = event.code;
+function isWallAtPosition(x, z) {
+  const i = Math.floor(x / WALL_WIDTH);
+  const j = Math.floor(z / WALL_WIDTH);
 
-  // Get the direction that the camera is pointing in
+  if (i < 0 || i >= maze.length || j < 0 || j >= maze[0].length) {
+    return true;
+  }
+
+  return maze[i][j] === 1;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
   const direction = new THREE.Vector3();
   camera.getWorldDirection(direction);
   direction.y = 0;
+  direction.normalize();
 
   const right = new THREE.Vector3();
-  right.crossVectors(new THREE.Vector3(0, 1, 0), direction);
+  right.crossVectors(direction, new THREE.Vector3(0, 1, 0)).normalize();
 
-  switch (keyCode) {
-    case "KeyW":
-      // forwards
-      camera.position.add(direction.multiplyScalar(SPEED));
-      break;
-    case "KeyS":
-      // backwards
-      camera.position.sub(direction.multiplyScalar(SPEED));
-      break;
-    case "KeyA":
-      // left
-      camera.position.add(right.multiplyScalar(SPEED));
-      break;
-    case "KeyD":
-      // right
-      camera.position.sub(right.multiplyScalar(SPEED));
-      break;
+  const moveVector = new THREE.Vector3();
+
+  if (moveForward) moveVector.add(direction);
+  if (moveBackward) moveVector.sub(direction);
+  if (moveLeft) moveVector.sub(right);
+  if (moveRight) moveVector.add(right);
+
+  if (moveVector.lengthSq() > 0) {
+    moveVector.normalize().multiplyScalar(SPEED);
+
+    const newX = camera.position.x + moveVector.x;
+    const newZ = camera.position.z + moveVector.z;
+
+    const buffer = 1.5;
+
+    if (
+      !isWallAtPosition(newX + buffer, newZ + buffer) &&
+      !isWallAtPosition(newX - buffer, newZ + buffer) &&
+      !isWallAtPosition(newX + buffer, newZ - buffer) &&
+      !isWallAtPosition(newX - buffer, newZ - buffer)
+    ) {
+      camera.position.x = newX;
+      camera.position.z = newZ;
+    }
   }
+
+  skybox.rotation.y += 0.0001;
+  renderer.render(scene, camera);
 }
 
 init();
